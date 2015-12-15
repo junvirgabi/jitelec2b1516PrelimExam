7f98b42fc63a54bb26150ee475a394711e96bd11
junvirgabi <junvir12@gmail.com>
2015-12-15 17:20:58 +0800

    changed the id implemented seekbar added functionalities added handler added seekbar

--- app/src/main/java/tech/sidespell/prelimexam/MainActivity.java
+++ app/src/main/java/tech/sidespell/prelimexam/MainActivity.java
@@ -1,21 +1,98 @@
 package tech.sidespell.prelimexam;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView mTvTime;
+    private ToggleButton mBtnSwitch;
+    private RadioButton rbnPlus;
+    private RadioButton rbnMin;
+    private RadioGroup rbnGroup;
+    private SeekBar seekBar;
+
+
+
+    private long timeRemaining = 10000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mTvTime = (TextView) findViewById(R.id.txtVal);
+        mBtnSwitch = (ToggleButton) findViewById(R.id.btnPeform);
+        rbnPlus = (RadioButton)findViewById(R.id.rbnInc);
+        rbnMin = (RadioButton)findViewById(R.id.rbnDec);
+        seekBar =(SeekBar)findViewById(R.id.seekBar);
+
+        mTvTime.setText(seekBar.getProgress());
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            int progress = 0;
+
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+
+                progress = progresValue;
+
+                Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
+
+            }
+
+
+            @Override
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+                Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+
+            }
+
+
+            @Override
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+                mTvTime.setText("Covered: " + progress + "/" + seekBar.getMax());
+
+                Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+        final Handler handler = new Handler();
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                timeRemaining -= 1000;
+                mTvTime.setText(timeRemaining + "");
+
+                if (timeRemaining > 0) {
+                    handler.postDelayed(this, 1000);
+                }
+            }
+        };
+
+        handler.postDelayed(runnable, 1000);
     }
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -37,5 +114,23 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onrbnGroup(View view) {
+
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.rbnInc:
+                if (checked)
+
+                    break;
+            case R.id.rbnDec:
+                if (checked)
+                    // Ninjas rule
+                    break;
+        }
     }
 }
